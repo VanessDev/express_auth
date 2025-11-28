ced8519196d4b729b93fb55d7eaa66336a34f952
VanessDev <vanessa.biamonti@gmail.com>
2025-11-28 10:06:21 +0100

    commit12

--- front/src/components/LoginForm.jsx
+++ front/src/components/LoginForm.jsx
@@ -2,14 +2,73 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { login } from "../services/api";
 
+function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-function LoginForm () {
-return (
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    setLoading(true);
+    setMessage("");
+    try {
+      const result = await login(email, password);
+      //je stocke mon token dans le localStorage
+      localStorage.setItem('token',result.token);
+      setMessage('connexion reussie');
+    } catch (error) {
+      setMessage(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
     <>
-    
-    
+      <h2>page login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            disabled={loading}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">MDP:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+            disabled={loading}
+          />
+        </div>
+        <button type="submit" disabled={loading}>
+          {loading ? "Chargement" : "se connecter"}
+        </button>
+      </form>
+
+      {/* afficher les message de succes et d'erreurs */}
+      {message}
+
+      <div>
+        pas de compte ? <Link to={"/register"}>S'inscrire</Link>
+      </div>
     </>
-)
+  );
 }
 
-export default LoginForm
+export default LoginForm;
